d8d29c69708bcc8ce71c248a4d0cc0c4c96cf2f3
GRGEznk <artes.visuales.astigmatic@gmail.com>
2026-02-05 18:36:58 -0500

    ux quiz

--- src/routes/quizRoutes.ts
+++ src/routes/quizRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { startSession, saveAnswers, getSession, getAllSessions } from '../controllers/quizController';
+import { startSession, saveAnswers, getSession, getAllSessions, linkSessionWithUser } from '../controllers/quizController';
 import { getMatches } from '../controllers/resultsController';
 
 const router = Router();
 
 router.post('/start', startSession);
 router.post('/answers', saveAnswers);
+router.post('/link-session', linkSessionWithUser);
 router.get('/session/:id', getSession);
 router.get('/session/:id/matches', getMatches);
 router.get('/sessions', getAllSessions);
