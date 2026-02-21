f9a2bf5d83bfc7996baff077c3a645985d119daf
GRGEznk <artes.visuales.astigmatic@gmail.com>
2026-02-21 00:51:06 -0500

    reduce pass

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,28 +4,20 @@ export const validatePassword = (password: string) => {
     const upperCount = (password.match(/[A-Z]/g) || []).length;
     const lowerCount = (password.match(/[a-z]/g) || []).length;
     const numberCount = (password.match(/[0-9]/g) || []).length;
-    const signCount = (password.match(/[+\-*/]/g) || []).length;
-    const symbolCount = (password.match(/[^a-zA-Z0-9+\-*/]/g) || []).length;
 
-
-    if (upperCount < 2) {
-        return { isValid: false, error: "Mínimo 2 letras mayúsculas" };
+   
+    if (upperCount < 1) {
+        return { isValid: false, error: "Mínimo 1 letra mayúscula" };
     }
-    if (lowerCount < 2) {
-        return { isValid: false, error: "Mínimo 2 letras minúsculas" };
+    if (lowerCount < 1) {
+        return { isValid: false, error: "Mínimo 1 letra minúscula" };
     }
-    if (numberCount < 2) {
-        return { isValid: false, error: "Mínimo 2 dígitos numéricos" };
+    if (numberCount < 1) {
+        return { isValid: false, error: "Mínimo 1 dígito numérico" };
     }
-    if (signCount < 2) {
-        return { isValid: false, error: "Mínimo 2 signos (+, -, *, /)" };
+    if (password.length < 6) {
+        return { isValid: false, error: "Mínimo 6 caracteres" };
     }
-    if (symbolCount < 2) {
-        return { isValid: false, error: "Mínimo 2 símbolos (otros caracteres especiales)" };
-    }
-    if (password.length < 12) {
-        return { isValid: false, error: "Mínimo 12 caracteres" };
-    }
-
     return { isValid: true, error: null };
 };
+
